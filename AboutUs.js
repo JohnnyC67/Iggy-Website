4bdd0e0c85b878e2e5a58892bc8f933f1b6ba53b
Johnny Cales <cales.17@osu.edu>
2024-06-02 15:46:08 -0400

    First commit

---
+++ AboutUs.js
@@ -0,0 +1,20 @@
+function addEvent(title, imageSrc, entry){
+    //Add title to dropdown menu
+    //Add image to document
+    //Add text next to added image
+    return;
+}
+
+function viewEvent(eventID){
+
+}
+
+function deleteEvent(eventID){
+
+}
+
+function convertToEvents(){
+    //Acquire events data from database
+    //Parse information into HTML elements
+    //Display data onto HTML page
+}
---
+++ experiment.js
@@ -0,0 +1,38 @@
+function toAboutUs(){
+    console.log("Navigating...");
+    document.location = "aboutUs.html";
+    console.log("Finished!");
+}
+
+function toHoursAndLocation(){
+    console.log("Navigating...");
+    document.location = "hoursAndLocation.html";
+    console.log("Finished!");
+}
+
+function toMain(){
+    console.log("Navigating...");
+    document.location = "menu.html";
+    console.log("Finished!");
+}
+
+function increment(value) {
+    value++;
+    alert(value);
+    console.log(value);
+    return value;
+}
+
+food_1 = {
+    value: 0
+};
+food_2 = {
+    value: 0
+};
+food_3 = {
+    value: 0
+};
+food_4 = {
+    value: 0
+};
+console.log("Hey it worked!");
