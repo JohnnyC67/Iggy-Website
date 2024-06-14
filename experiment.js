986f058c48f8a7fe2f9367c4c80a862af536152f
Johnny Cales <cales.17@osu.edu>
2024-06-14 12:08:33 -0400

    Made more layout adjustments. Still working on positioning of top section elements.

--- experiment.js
+++ experiment.js
@@ -16,6 +16,18 @@ function toMain(){
     console.log("Finished!");
 }
 
+function toInstagram(){
+    console.log("Navigating...");
+    document.location = "https://www.instagram.com/iggysbar/";
+    console.log("Finished!");
+}
+
+function toFacebook(){
+    console.log("Navigating...");
+    document.location = "https://www.facebook.com/iggyslakewood/";
+    console.log("Finished!");
+}
+
 function increment(value) {
     value++;
     alert(value);
